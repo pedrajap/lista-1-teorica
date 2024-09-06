5fd8f13afcbc85170396f84d5d2e9595065d0ccb
pedrajap <giu.pedraja@gmail.com>
2024-09-06 02:39:22 -0300

    Add files via upload

---
+++ lista1ex1a.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+//trocar o tipo da variavel de int para float e mudar o printf de %d para %f
+int main() {
+
+    float n = 4000000000;
+    printf("%f", n);
+
+    return 0;
+}
