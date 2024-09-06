2db9e4307fb6807a49d849b48f3edb863684e057
pedrajap <giu.pedraja@gmail.com>
2024-09-06 02:55:57 -0300

    Add files via upload

---
+++ lista1ex1c.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+//adicionar /n na funcao printf para melhorar a legibilidade e criar duas variaveis com os valores
+int main() {
+
+    int a = 4, b = 2;
+    printf("%d\n", a);
+    printf("%d\n", b);
+
+    return 0;
+}
